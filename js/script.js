097a5661d0f8cff7348983a4820c6fe8f62ec39b
cif95 <vivianacifarelli9@gmail.com>
2022-03-03 15:35:41 +0100

    completed exercise palindrome

--- js/script.js
+++ js/script.js
@@ -3,6 +3,32 @@
 // Creare una funzione per capire se la parola inserita è palindroma
 
 
+const userWord = prompt('Inserisci una parola');
+
+
+
+/**Function that checks if a word is palindrome
+ * 
+ * @param {*} word word to be checked
+ * @returns true if the word is palindrome, otherwise returs false.
+ */
+function isPalindrome (word) {
+	const wordArray = Array.from(word);
+	console.log(wordArray);
+	const reversedWordArray = [...wordArray].reverse();
+	console.log(reversedWordArray);
+	const reversedWord = reversedWordArray.join('');
+	console.log(reversedWord);
+	if ( word === reversedWord) {
+		return true
+	}
+	return false
+}
+
+console.log(isPalindrome(userWord));
+
+
+
 
 // Pari e Dispari
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
